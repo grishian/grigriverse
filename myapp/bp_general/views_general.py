e93405ba6eb79000ecc0f8b60451165a11e312a4
grishian <grishian@hotmail.com>
2022-12-03 19:29:17 +0100

    nog vlug wa dummy pagina's gemaakt

--- myapp/bp_general/views_general.py
+++ myapp/bp_general/views_general.py
@@ -6,3 +6,18 @@ from flask import render_template, url_for, redirect, request, flash, abort
 def do_home():
     return render_template('general/home.html')
 
+@bp_general.route('/forum')
+def do_forum():
+    return render_template('general/forum.html')
+
+@bp_general.route('/tutorials')
+def do_tutorials():
+    return render_template('general/tutorials.html')
+
+@bp_general.route('/events')
+def do_events():
+    return render_template('general/events.html')
+
+@bp_general.route('/contact')
+def do_contact():
+    return render_template('general/contact.html')
